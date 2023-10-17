24e3d6fc01cf232d166d0bb1874711731d42df91
WearingCucumber <257654131@qq.com>
2023-10-17 20:18:12 +0800

    配置文件增加/member 修改启动日志，修改logbak日志

--- train-member/src/main/java/com/study/train/member/config/MemberApplication.java
+++ train-member/src/main/java/com/study/train/member/config/MemberApplication.java
@@ -1,13 +1,19 @@
 package com.study.train.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.core.env.ConfigurableEnvironment;
+@Slf4j
 @SpringBootApplication
 @ComponentScan("com.study.train")
 public class MemberApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class,args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("host: \t http://127.0.0.1:{}",env.getProperty("server.port"));
+
     }
 }
