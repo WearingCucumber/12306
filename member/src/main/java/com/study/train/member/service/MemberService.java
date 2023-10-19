948bc1915581877e09afc6915c944537e50454f6
WearingCucumber <257654131@qq.com>
2023-10-19 11:29:00 +0800

    实现简单的注册接口

--- member/src/main/java/com/study/train/member/service/MemberService.java
+++ member/src/main/java/com/study/train/member/service/MemberService.java
@@ -2,4 +2,5 @@ package com.study.train.member.service;
 
 public interface MemberService {
     int count();
+    long register(String mobile);
 }
--- member/src/main/java/com/study/train/member/service/MemberServiceImpl.java
+++ member/src/main/java/com/study/train/member/service/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package com.study.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.study.train.member.domain.Member;
+import com.study.train.member.domain.MemberExample;
 import com.study.train.member.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberServiceImpl implements MemberService{
@@ -12,6 +17,23 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+
+    }
+
+    @Override
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(list))
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已经注册");
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
 
     }
 }
