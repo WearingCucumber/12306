da8d687ab6309ff7ff83f2ad886b379eb70236c6
WearingCucumber <257654131@qq.com>
2023-10-21 01:58:30 +0800

    添加路由拦截 对所有需要登录的页面进行拦截 如果没有登录则跳转到登录页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from "@/store";
+import {notification} from "ant-design-vue";
 
 
 const routes = [
@@ -11,13 +13,34 @@ const routes = [
   {
     path:'/',
     name: 'main',
-    component: ()=>import('../views/MainView.vue')
+    component: ()=>import('../views/MainView.vue'),
+    meta:{
+      loginRequire:true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+router.beforeEach((to,from,next)=>{
+  //要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item){
+    console.log(item,"是否需要登陆校验：",item.meta.loginRequire || false);
+    return item.meta.loginRequire
+  })){
+    const  member = store.state.member;
+    console.log("页面登录校验开始");
+    if (!member.token){
+      notification.error({ description: '登录状态异常,请重新登录' });
+      next('/login')
+    }else {
+      next();
+    }
+  }else {
+    next();
+  }
 })
 
 export default router
