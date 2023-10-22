53613ff4ab15321153c50524b88b247d8ceb54a9
WearingCucumber <257654131@qq.com>
2023-10-22 19:56:12 +0800

    使用@Min注解代替@NotNull在不携带对应参数时默认值为0的问题 @NotNull(message = "页码不能为空") @Min(value=1,message = "页码不能为空")

--- common/src/main/java/com/study/train/common/req/PageReq.java
+++ common/src/main/java/com/study/train/common/req/PageReq.java
@@ -1,12 +1,12 @@
 package com.study.train.common.req;
 
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 
 public class PageReq {
-    @NotNull(message = "页码不能为空")
+    @Min(value=1,message = "页码不能为空")
     private int page;
-    @NotNull(message = "每页条数不能为空")
+    @Min(value=1,message = "每页条数不能为空")
     @Max(value = 100,message = "每页条数不能超过100")
     private int pageSize;
 
